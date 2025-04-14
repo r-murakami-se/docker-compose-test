38d11db08e3bbbfad3d3e9dd372e14fe4af8d692
risa <r.murakami.se@gmail.com>
2025-04-14 15:28:27 +0900

    dcmdb構成見直し

--- docker/dcmdb/init.sh
+++ docker/dcmdb/init.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
 
-# MySQL起動（バックグラウンド）
-/usr/local/mysql/bin/mysqld_safe --datadir=/usr/local/mysql/data &
+# 初期化チェック（data/mysql フォルダが空なら初期化）
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    /usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+    for i in {1..30}; do
+        if /usr/bin/mysqladmin ping --silent; then
+            break
+        fi
+        sleep 1
+    done
 
-# 起動待ち（最大30秒）
-for i in {1..30}; do
-  if /usr/local/mysql/bin/mysqladmin ping --silent; then
-    break
-  fi
-  sleep 1
-done
+    # rootパスワード設定
+    mysqladmin -u root password "${MYSQL_ROOT_PASSWORD}"
 
-# rootパスワードを設定
-/usr/local/mysql/bin/mysqladmin -u root password "$MYSQL_ROOT_PASSWORD"
-
-# どこからでも接続できる root ユーザー追加
-/usr/local/mysql/bin/mysql -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOS
+    # どこからでも接続できる root ユーザー追加
+    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOS
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOS
 
-# データベース作成
-/usr/local/mysql/bin/mysql -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOF
+    # データベース作成
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" <<EOF
 CREATE DATABASE IF NOT EXISTS cakephptest DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE DATABASE IF NOT EXISTS masterkanri DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 EOF
 
-# 初期SQL投入
-if [ -f /docker-entrypoint-initdb.d/01_cakephptest.sql ]; then
-  /usr/local/mysql/bin/mysql -uroot -p"$MYSQL_ROOT_PASSWORD" cakephptest < /docker-entrypoint-initdb.d/01_cakephptest.sql
+    # 初期SQL投入
+    [ -f /docker-entrypoint-initdb.d/01_cakephptest.sql ] && \
+        mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" cakephptest < /docker-entrypoint-initdb.d/01_cakephptest.sql
+
+    [ -f /docker-entrypoint-initdb.d/02_masterkanri.sql ] && \
+        mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" masterkanri < /docker-entrypoint-initdb.d/02_masterkanri.sql
+
+    # MySQL停止
+    pkill mysqld
+    sleep 3
 fi
 
-if [ -f /docker-entrypoint-initdb.d/02_masterkanri.sql ]; then
-  /usr/local/mysql/bin/mysql -uroot -p"$MYSQL_ROOT_PASSWORD" masterkanri < /docker-entrypoint-initdb.d/02_masterkanri.sql
-fi
-
-# MySQL停止
-pkill mysqld
-
-sleep 3
-
-# MySQLをフォアグラウンドで起動
-exec /usr/local/mysql/bin/mysqld_safe --datadir=/usr/local/mysql/data
+# Foregroundで起動
+exec /usr/bin/mysqld_safe --datadir=/var/lib/mysql
