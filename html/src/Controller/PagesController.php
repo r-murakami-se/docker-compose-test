2a341e6818a5ed6a0f470e4493dedc69bf1f1225
risa <r.murakami.se@gmail.com>
2025-04-21 17:35:36 +0900

    コンテナ名変更

--- html/src/Controller/PagesController.php
+++ html/src/Controller/PagesController.php
@@ -40,7 +40,7 @@ class PagesController extends AppController
      */
     public function display(...$path)
     {
-        // $con = ssh2_connect('dcmfile', 22);
+        // $con = ssh2_connect('file', 22);
         // ssh2_auth_password($con, 'shadmin', 'password');
         // $stream = ssh2_exec($con, 'ls /home/shadmin/');
         // stream_set_blocking($stream, true);
@@ -49,7 +49,7 @@ class PagesController extends AppController
         // exit;
 
         // ob_start();
-        // system('scp -i /usr/share/httpd/.ssh/id_rsa /var/www/html/config/local/app.php shadmin@dcmfile:/home/shadmin/ 2>&1', $status);
+        // system('scp -i /usr/share/httpd/.ssh/id_rsa /var/www/html/config/local/app.php shadmin@file:/home/shadmin/ 2>&1', $status);
         // $output = ob_get_clean();
         // dd($output, $status);
         // exit;
--- setup.bat
+++ setup.bat
@@ -1,10 +1,12 @@
-docker compose build dcmweb
-docker compose up -d dcmweb
-docker cp dcm-web:/root/.ssh/id_rsa.pub ./docker/dcmfile/authorized_keys
-docker compose build dcmfile
+docker compose build db1
+docker compose build db2
+docker compose build web
+docker compose up -d web
+docker cp web:/root/.ssh/id_rsa.pub ./docker/dcmfile/authorized_keys
+docker compose build file
 docker compose up -d
 
-docker compose exec dcmweb bash -c "ssh-keyscan -H dcmfile >> /root/.ssh/known_hosts"
-docker compose exec dcmweb bash -c "cp /root/.ssh/known_hosts /usr/share/httpd/.ssh/known_hosts"
-docker compose exec dcmweb bash -c "chmod 600 /usr/share/httpd/.ssh/known_hosts"
-docker compose exec dcmweb bash -c "chown -R apache:apache /usr/share/httpd/.ssh"
+docker compose exec web bash -c "ssh-keyscan -H file >> /root/.ssh/known_hosts"
+docker compose exec web bash -c "cp /root/.ssh/known_hosts /usr/share/httpd/.ssh/known_hosts"
+docker compose exec web bash -c "chmod 600 /usr/share/httpd/.ssh/known_hosts"
+docker compose exec web bash -c "chown -R apache:apache /usr/share/httpd/.ssh"
